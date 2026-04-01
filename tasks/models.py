62e3ee9d2f2db01a58b7713b7a87d7b6796a2109
devpatel-cloud <devsangani1234@gmail.com>
2026-04-01 14:35:07 +0530

    new change

--- tasks/models.py
+++ tasks/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
-
 class Status(models.TextChoices):
     UNSTARTED = 'u', "Not started yet"
     ONGOING = 'o', "Ongoing"
@@ -10,8 +8,17 @@ class Status(models.TextChoices):
 
 
 class Task(models.Model):
-    name = models.CharField(verbose_name="Task name", max_length=65, unique=True)
-    status = models.CharField(verbose_name="Task status", max_length=1, choices=Status.choices)
+    name = models.CharField(
+        verbose_name="Task name",
+        max_length=65,
+        unique=True
+    )
+    status = models.CharField(
+        verbose_name="Task status",
+        max_length=1,
+        choices=Status.choices,
+        default=Status.UNSTARTED   # ✅ FIX ADDED
+    )
 
     def __str__(self):
         return self.name
